4616faa524359cfc06ce57aea7a44349f9003605
Random Guy <chrsbuitendijk@gmail.com>
2025-07-02 09:34:32 +0200

    added docstrings to throw.py

--- Pylibs/throw/throw.py
+++ Pylibs/throw/throw.py
@@ -1,11 +1,18 @@
 def throwError(x):
+    """ Simply throws an error.
+    
+        prints the error, then quits"""
     print(x)
     quit()
 
 def throwErrorCode(y):
+    """ Simply throws an error code.
+    
+        only integers work for this function. Prints the error code and then quits"""
     print(int(y))
     quit()
 
 def throwErrorWithCode(x, y):
+    """throws an error with an added error code"""
     print(f"{x}\nExiting with error code {y}")
     quit()
